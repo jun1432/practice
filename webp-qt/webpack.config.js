8fb2bd270a9826dbd752aed97481eb9a29d5a03e
Junry Zabate <jzabajte@melistechnology.com>
2018-12-19 13:56:27 +0800

    commits all the practice folders

---
+++ webp-qt/webpack.config.js
@@ -0,0 +1,39 @@
+const HTMLWebPackPlugin     = require("html-webpack-plugin"),
+      MiniCssExtractPlugin  = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: '/\.html$/',
+        use: [
+          {
+            loader: "hmtl-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunckFilename: "[id].css"
+    })
+  ]
+};
